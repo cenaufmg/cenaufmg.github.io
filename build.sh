821430d139ffb15d189946f0108391370acec7c7
cenaufmg <cenaufmg@gmail.com>
2019-12-05 01:29:10 -0300

    Initial commit

---
+++ build.sh
@@ -0,0 +1,30 @@
+cat ./templates/page-top-br.html > ./mission-br.html
+cat ./templates/page-mission-br.html >> ./mission-br.html
+cat ./templates/page-bottom-br.html >> ./mission-br.html
+
+cat ./mission-br.html > ./index.html
+
+cat ./templates/page-top-br.html > ./team-br.html
+cat ./templates/page-team-br.html >> ./team-br.html
+cat ./templates/page-bottom-br.html >> ./team-br.html
+
+cat ./templates/page-top-br.html > ./ufmg-br.html
+cat ./templates/page-ufmg-br.html >> ./ufmg-br.html
+cat ./templates/page-bottom-br.html >> ./ufmg-br.html
+
+cat ./templates/page-top-br.html > ./news-br.html
+cat ./templates/page-news-br.html >> ./news-br.html
+cat ./templates/page-bottom-br.html >> ./news-br.html
+
+cat ./templates/page-top-br.html > ./agreements-usa-br.html
+cat ./templates/page-agreements-usa-br.html >> ./agreements-usa-br.html
+cat ./templates/page-bottom-br.html >> ./agreements-usa-br.html
+
+cat ./templates/page-top-br.html > ./agreements-canada-br.html
+cat ./templates/page-agreements-canada-br.html >> ./agreements-canada-br.html
+cat ./templates/page-bottom-br.html >> ./agreements-canada-br.html
+
+cat ./templates/page-top-br.html > ./links-br.html
+cat ./templates/page-links-br.html >> ./links-br.html
+cat ./templates/page-bottom-br.html >> ./links-br.html
+
